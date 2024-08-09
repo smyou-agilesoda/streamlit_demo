81c290c4f2c243ab57dae2bf9d71558732e710e5
ysmNew <ysmlovely@naver.com>
2024-08-09 17:01:44 +0900

    add pdf viewer

--- page/test.py
+++ page/test.py
@@ -7,6 +7,7 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain_openai import ChatOpenAI
 import streamlit as st
+from streamlit_pdf_viewer import pdf_viewer
 
 # TODO)
 # 추천질문 집어넣기
@@ -14,20 +15,32 @@ import streamlit as st
 # 요약문
 # pdf뷰어
 
-class StreamHandler(BaseCallbackHandler):
-    def __init__(self, container, initial_text=""):
-        self.container = container
-        self.text = initial_text
+# class StreamHandler(BaseCallbackHandler):
+#     def __init__(self, container, initial_text=""):
+#         self.container = container
+#         self.text = initial_text
 
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text += token
-        self.container.markdown(self.text)
+#     def on_llm_new_token(self, token: str, **kwargs) -> None:
+#         self.text += token
+#         self.container.markdown(self.text)
+if 'doc_id' not in st.session_state:
+    st.session_state['doc_id'] = None
+if 'uploaded' not in st.session_state:
+    st.session_state['uploaded'] = False
+if 'binary' not in st.session_state:
+    st.session_state['binary'] = None
+
+def new_file():
+    st.session_state['doc_id'] = None
+    st.session_state['uploaded'] = True
+    st.session_state['binary'] = None
 
 cfg = RunnableConfig()
 cfg["configurable"] = {"session_id": "any"}
 
 # Set up memory
 msgs = StreamlitChatMessageHistory(key="langchain_messages")
+# 질문 답변용 프롬프트 수정 필요
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "한글로 간결하게 답변하세요."),
@@ -35,7 +48,17 @@ prompt = ChatPromptTemplate.from_messages(
         ("human", "{question}"),
     ]
 )
+llm = ChatOpenAI()
+chain = prompt | llm
+chain_with_history = RunnableWithMessageHistory(
+    chain,
+    lambda session_id: msgs,
+    input_messages_key="question",
+    history_messages_key="history",
+)
 
+# TODO) 추천질문 추가
+# tracing 관련 에러 체크
 @st.dialog("Ask your question", width="large")
 def ask_question():
     if "messages" not in st.session_state:
@@ -45,7 +68,6 @@ def ask_question():
     for msg in st.session_state.messages:
         with st.chat_message(msg.role):
             st.markdown(msg.content)
-        # st.chat_message(msg.role).write(msg.content)
 
     # 질문 입력 전까지 placeholder 숨기기
     if "chat_before" not in st.session_state: 
@@ -66,21 +88,12 @@ def ask_question():
         with question_placeholder:
             st.markdown(user_input)
         # with answer_placeholder:
-        #     response = user_input
+        #     response = user_input + user_input
         #     st.markdown(user_input)
         #     st.session_state.messages.append(
-        #         ChatMessage(role="assistant", content=response+response)
+        #         ChatMessage(role="assistant", content=response)
         #     )
         with answer_placeholder:
-            # stream_handler = StreamHandler(st.empty())
-            llm = ChatOpenAI()#streaming=True)#, callbacks=[stream_handler])
-            chain = prompt | llm
-            chain_with_history = RunnableWithMessageHistory(
-                chain,
-                lambda session_id: msgs,
-                input_messages_key="question",
-                history_messages_key="history",
-            )
             response = chain_with_history.stream({"question": user_input}, cfg)
             response = st.write_stream(response)
             st.session_state.messages.append(
@@ -92,7 +105,11 @@ left_col, right_col = st.columns([1, 1])
 with left_col:
     st.subheader("pdf 요약")
     file_uploader_placeholder = st.empty()
-    uploaded_file = file_uploader_placeholder.file_uploader("파일 업로드", type=["pdf"])
+    uploaded_file = file_uploader_placeholder.file_uploader(
+        "파일 업로드", 
+        type=["pdf"],
+        on_change=new_file,
+        )
             
     if uploaded_file is not None:
         # 업로드 파일 임시 저장
@@ -102,6 +119,12 @@ with left_col:
         
         st.write("업로드된 파일:", uploaded_file.name)
         
+        if not st.session_state['binary']:
+            with (st.spinner('Reading file...')):
+                binary = uploaded_file.getvalue()
+                st.session_state['binary'] = binary
+                
+
         # parsing
         
         
@@ -118,3 +141,6 @@ with right_col:
     # 파일 업로드 전 버튼 비활성화? 숨기기?
     if st.button("modal test"):
         ask_question()
+    if st.session_state['binary']:
+        with (st.spinner("Rendering PDF document")):
+            pdf_viewer(st.session_state['binary'])
